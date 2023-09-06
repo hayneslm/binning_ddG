aeec46fe2047e9ec0c66c916ff5c6c331b700903
Laura Haynes <hayneslm@umich.edu>
2023-09-06 15:48:48 -0400

    staring to incorporate number of contacts

--- 3q02.R
+++ 3q02.R
@@ -5,10 +5,11 @@ data_0h_func = data_0h %>% filter(log2FoldChange > 0)
 data_0h_nf = data_0h %>%  filter(log2FoldChange <= 0)
 
 eq02_func = full_join(data_0h_func, ddg_eq02_cut) %>% na.omit() %>% select(bin)
+eq02_func_list = full_join(data_0h_func, ddg_eq02_cut) %>% na.omit()
 eq02_func_table = table(eq02_func) %>% as.data.frame() %>% 
   mutate(fraction = Freq/sum(Freq))
 write_tsv(eq02_func_table, "eq02_func.xls")
-# plot(eq02_func_table)
+plot(eq02_func_table)
 
 
 eq02_nf = full_join(data_0h_nf, ddg_eq02_cut) %>% na.omit() %>% select(bin)
@@ -28,15 +29,39 @@ func_48h_active = full_join(ddg_eq02_cut, func48) %>%
 table48h_active = table(func_48h_active) %>% as.data.frame()%>% 
   mutate(fraction = Freq/sum(Freq))
 write_tsv(table48h_active, "eq02_func_active.xls")
-plot(table_48h)
+# plot(table_48h)
 
 func_48h_latent = full_join(ddg_eq02_cut, func48) %>% 
   filter(log2FoldChange <= 0) %>% na.omit() %>% 
   select(bin)
+func_48h_latent_list = full_join(ddg_eq02_cut, func48) %>% 
+  filter(log2FoldChange <= 0) %>% na.omit()
+func_48h_latent_list_H1 = func_48h_latent_list %>% filter(ddG<0.8)
+func_48h_latent_list_H2 = func_48h_latent_list %>% filter(ddG>=0.8)
+H1_postions=unique(func_48h_latent_list_H1$Pos) %>% as.data.frame()
+H2_postions=unique(func_48h_latent_list_H2$Pos) %>% as.data.frame()
+H1_unique = anti_join(H1_postions, H2_postions) %>% 
+  rename(position = ".")
+H2_unique = anti_join(H2_postions, H1_postions)%>% 
+  rename(position = ".")
+
+# Import contacts table the jaie made for each position
+contacts = read_csv("contacts_table.csv", col_names = FALSE) %>% 
+  rename(position = X1, cutoff_1 = X2, cutoff_2 = X3)
+
+
+
+
+
+
+
+
+
+
 table48h_latent = table(func_48h_latent) %>% as.data.frame()%>% 
   mutate(fraction = Freq/sum(Freq))
 write_tsv(table48h_latent, "eq02_func_latent.xls")
-plot(table_48h)
+plot(table48h)
 
 
 
